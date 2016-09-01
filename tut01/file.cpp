ef5488bbb93a43a4dee1cfacef2473aea369a414
Aditya Atluri <Aditya.Atluri@amd.com>
2016-08-31 20:46:02 -0500

    Added sample which compiles and runs

---
+++ tut01/file.cpp
@@ -0,0 +1,33 @@
+
+#include <iostream>
+#include "clang/Driver/Options.h"
+#include "clang/AST/AST.h"
+#include "clang/AST/ASTContext.h"
+#include "clang/AST/ASTConsumer.h"
+#include "clang/AST/RecursiveASTVisitor.h"
+#include "clang/Frontend/ASTConsumers.h"
+#include "clang/Frontend/FrontendActions.h"
+#include "clang/Frontend/CompilerInstance.h"
+#include "clang/Tooling/CommonOptionsParser.h"
+#include "clang/Tooling/Tooling.h"
+#include "clang/Rewrite/Core/Rewriter.h"
+
+using namespace std;
+using namespace clang;
+using namespace clang::driver;
+using namespace clang::tooling;
+using namespace llvm;
+
+
+int main(int argc, const char **argv) {
+    cl::OptionCategory MyToolCategory("Example1");    
+    // parse the command-line args passed to your code
+    CommonOptionsParser op(argc, argv, MyToolCategory);        
+    // create a new Clang Tool instance (a LibTooling environment)
+    ClangTool Tool(op.getCompilations(), op.getSourcePathList());
+
+    // run the Clang Tool, creating a new FrontendAction (explained below)
+    return 0;
+}
+
+
